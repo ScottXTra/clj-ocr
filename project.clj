de7ead8ec38ebcbc4e791964da5907864db75ff0
Scott <sweselan@uoguelph.com>
2021-07-12 00:34:46 -0400

    Setup

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clj-ocr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure wrapper for Tesseract OCR"
+  :url "https://github.com/ScottXTra/clj-ocr"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [net.sourceforge.tess4j/tess4j "3.2.1"]]
   :repl-options {:init-ns clj-ocr.core})
