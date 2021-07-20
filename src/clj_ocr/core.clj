23b924c63788ec77a9fd7fe190e05635ec4ede3a
Scott <sweselan@uoguelph.com>
2021-07-19 22:57:43 -0400

    Added comment

--- src/clj_ocr/core.clj
+++ src/clj_ocr/core.clj
@@ -13,6 +13,7 @@
 (def instance
   (new Tesseract))
 ;; param 1 -> buffered image ( to read a file from disk to buffered image is (ImageIO/read (clojure.java.io/file "pic.png")) )
+;; param 2 -> Tesseract instance
 ;; returns -> text from the image 
 (defn do-ocr [bi tess-instance]
   (.doOCR tess-instance bi))
